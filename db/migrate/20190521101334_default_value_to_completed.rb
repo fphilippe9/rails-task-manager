b84bcf0c684cc32290e2839cae9cf5fd76db411a
francois philippe <fphilippe9@gmail.com>
2019-05-21 16:27:19 +0200

    good

---
+++ db/migrate/20190521101334_default_value_to_completed.rb
@@ -0,0 +1,6 @@
+class DefaultValueToCompleted < ActiveRecord::Migration[5.2]
+  def change
+    change_column :Tasks, :completed, :boolean, default: false
+
+  end
+end
---
+++ db/migrate/20190521111740_new_value.rb
@@ -0,0 +1,4 @@
+class NewValue < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
